508d1324ac11e57c4959b20a4b3c989d354a8ba9
Simone S. <simone.simonella@mm-one.com>
2024-02-06 14:58:28 +0100

    v2

--- base64.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-
-std::string base64_encode(char const* buf, unsigned int bufLen);
-std::vector<char> base64_decode(std::string const&);
--- main.cpp
+++ main.cpp
@@ -1,79 +1,78 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <random>
-#include <sstream>
 
-#include "base64.h"
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace asio = boost::asio;           // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-const std::string SERVER_IP = "127.0.0.1";
+const std::string SERVER_IP = "localhost";
 const int SERVER_PORT = 6666;
 
-std::string generate_nonce() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 15);
+int main()
+{
+    try
+    {
+        // Check command line arguments.
+        std::string const host = SERVER_IP;
+        auto const port = std::to_string(SERVER_PORT);
+        auto const text = "Hello, WebSocket!";
 
-    std::stringstream ss;
-    for (int i = 0; i < 16; ++i) {
-        ss << std::hex << dis(gen);
+        // The io_context is required for all I/O
+        asio::io_context ioc;
+
+        // These objects perform our I/O
+        tcp::resolver resolver{ioc};
+        beast::websocket::stream<tcp::socket> ws{ioc};
+
+        // Look up the domain name
+        auto const results = resolver.resolve(host, port);
+
+        // Make the connection on the IP address we get from a lookup
+        auto ep = asio::connect(ws.next_layer(), results);
+
+        // Update the host_ string. This will provide the value of the
+        // Host HTTP header during the WebSocket handshake.
+        // See https://tools.ietf.org/html/rfc7230#section-5.4
+        std::string host_ = host + ':' + std::to_string(ep.port());
+
+        // Set a decorator to change the User-Agent of the handshake
+        ws.set_option(beast::websocket::stream_base::decorator(
+            [](beast::websocket::request_type& req)
+            {
+                req.set(beast::http::field::user_agent,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                        " websocket-client-coro");
+            }));
+
+        // Perform the websocket handshake
+        ws.handshake(host_, "/");
+
+        // Send the message
+        ws.write(asio::buffer(std::string(text)));
+
+        // This buffer will hold the incoming message
+        beast::flat_buffer buffer;
+
+        // Read a message into our buffer
+        ws.read(buffer);
+
+        // Close the WebSocket connection
+        ws.close(beast::websocket::close_code::normal);
+
+        // If we get here then the connection is closed gracefully
+
+        // The make_printable() function helps print a ConstBufferSequence
+        std::cout << beast::make_printable(buffer.data()) << std::endl;
     }
-
-    return ss.str();
+    catch(std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
-
-int main() {
-    int clientSocket;
-    struct sockaddr_in serverAddr;
-    
-    // Create a socket
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        std::cerr << "Error creating socket\n";
-        return 1;
-    }
-
-    // Set up server address
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(SERVER_PORT);
-    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP.c_str());
-
-    // Connect to the server
-    if (connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Error connecting to server\n";
-        return 1;
-    }
-
-    std::string nonce = generate_nonce();
-    std::string base64_nonce = base64_encode(nonce.c_str(), nonce.length());
-
-    // Send WebSocket handshake directly
-    const std::string WS_HANDSHAKE = "GET / HTTP/1.1\r\n" +
-                                     std::string("Host: " + SERVER_IP + "\r\n") +
-                                     "Upgrade: websocket\r\n" +
-                                     "Connection: Upgrade\r\n" +
-                                     "Sec-WebSocket-Key: " + base64_nonce + "\r\n" +
-                                     "Sec-WebSocket-Version: 13\r\n\r\n";
-    if (send(clientSocket, WS_HANDSHAKE.c_str(), WS_HANDSHAKE.length(), 0) == -1) {
-        std::cerr << "Error sending WebSocket handshake request\n";
-        return 1;
-    }
-
-    // Receive and print response from the server
-    char buffer[1024];
-    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesRead == -1) {
-        std::cerr << "Error receiving response from server\n";
-        return 1;
-    }
-    buffer[bytesRead] = '\0';
-    std::cout << "Received response from server:\n" << buffer << std::endl;
-
-    // Close the socket
-    close(clientSocket);
-
-    return 0;
-}
-
